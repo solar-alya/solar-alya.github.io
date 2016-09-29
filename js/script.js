f094ba6ad3ab48559e0bca90965d468c66a00552
Alya Surikova <solar.alya@gmail.com>
2016-09-29 17:20:55 +0300

    fix menu

--- js/script.js
+++ js/script.js
@@ -5,11 +5,47 @@ function Menu() {
         $("#"+ItemData).fadeIn("slow");
         $(".menu__item").removeClass("activ__item");
         $(this).addClass("activ__item");
+        var windowWidth = $(window).width();
+        if(windowWidth <= 700) {
+            $('.menu__wrapper').toggleClass('menu__state');
+            $('.section').toggleClass('blur');
+            $('.menu__icon__burger').css('display','block');
+            $('.menu__icon__close').css('display','none');
+        }
     });
 }
 
 
+function sequentialEffects(elems, index) {
+  index = index || 0;
+  if (!elems[index]) return;
+  elems.eq(index).fadeIn("slow", function () {
+      sequentialEffects(elems, index + 1);
+    }
+  );
+}
 
+
+function MenuMobile() {
+    $('.menu__icon').on('click', function() {
+        $('.menu__wrapper').toggleClass('menu__state');
+        if($('.menu__wrapper').hasClass( 'menu__state' )){
+            $('.menu__icon__burger').css('display','none');
+            $('.menu__icon__close').css('display','block');
+        } else {
+            $('.menu__icon__burger').css('display','block');
+            $('.menu__icon__close').css('display','none');
+        }
+        $('.section').toggleClass('blur');
+
+        sequentialEffects($('.menu__item'));
+
+
+
+
+
+    });
+}
 
 
 $(document).ready(function() {
@@ -23,5 +59,6 @@ $(document).ready(function() {
   });
 
 Menu();
+MenuMobile();
 
 });
