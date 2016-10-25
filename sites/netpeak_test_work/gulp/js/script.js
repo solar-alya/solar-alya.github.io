ca469f4e99115f962833f08ab209631eabfcfc8b
Alya Surikova <solar.alya@gmail.com>
2016-10-25 19:16:20 +0300

    minify

---
+++ sites/netpeak_test_work/gulp/js/script.js
@@ -0,0 +1,66 @@
+var myScroll;
+
+function loaded () {
+    myScroll = new IScroll('.wrapper', {
+        scrollbars: true,
+        interactiveScrollbars: true,
+        mouseWheel: true,
+        fadeScrollbars: true,
+
+    });
+
+}
+
+function LoadMore() {
+    $("#loadMore").on('click', function (e){
+        e.preventDefault();
+        if ($(".group-card:hidden").length <= 4) {
+            $("#loadMore").fadeOut('slow');
+        }
+         $(".group-card:hidden").slice(0, 4).slideDown();
+         $("#to_top").fadeIn("slow");
+         setTimeout(function () {
+            myScroll.refresh();
+        }, 500);
+
+    });
+}
+
+function ToTop() {
+    $('#to_top').click(function () {
+        myScroll.scrollTo(0, 0, 1000);
+    });
+}
+function MenuMobile() {
+    $('.menu__icon').on('click', function() {
+        $('.menu').toggleClass('menu__state');
+        if($('.menu').hasClass( 'menu__state' )){
+            $('.menu').fadeIn('slow');
+            $('.menu__icon__burger').css('display','none');
+            $('.menu__icon__close').css('display','block');
+        } else {
+            $('.menu__icon__burger').css('display','block');
+            $('.menu__icon__close').css('display','none');
+            $('.menu').fadeOut('slow');
+        }
+    });
+}
+
+$(document).ready(function(){
+
+    $(".group-card").slice(0, 8).show();
+    loaded ();
+    LoadMore();
+    ToTop();
+    MenuMobile();
+
+    $(window).resize(function (){
+        var WindowWidth = $(window).width();
+        if(WindowWidth>640) {
+            $('.menu').css('display','flex');
+        }else {
+            $('.menu').css('display','none');
+        }
+    });
+
+})
--- sites/netpeak_test_work/gulpfile.js
+++ sites/netpeak_test_work/gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	csso = require('gulp-csso'),
+	uglify = require('gulp-uglify'),
+	minify = require('gulp-minify'),
 	jade = require('gulp-jade');
 
 gulp.task('jade', function() {
@@ -15,12 +17,18 @@ gulp.task('sass', function () {
 	.pipe(gulp.dest('./css')).pipe(csso())
 	.pipe(gulp.dest('css'));
 });
+gulp.task('js', function () {
+	gulp.src('gulp/js/*.js')
+	.pipe(uglify())
+	.pipe(gulp.dest('js'));
+});
 
 gulp.task('watch', function() {
 	gulp.watch('./gulp/*.jade', ['jade']);
 	gulp.watch('./gulp/includes/*.jade', ['jade']);
 	gulp.watch('./gulp/sass/*.scss', ['sass']);
 	gulp.watch('./gulp/sass/includes/*.scss', ['sass']);
+	gulp.watch('gulp/js/**/*.js', ['js']);
 });
 
 gulp.task('default', ['watch']);
